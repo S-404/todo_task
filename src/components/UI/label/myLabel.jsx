d0c389b1cb844d0a39793af98e38a9d589fa8892
S-404 <sudarev.andrey.i@gmail.com>
2021-11-20 20:48:21 +0300

    add myLabel ui

---
+++ src/components/UI/label/myLabel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './myLabel.module.css';
+const MyLabel = ({ labeltext, htmlFor }) => {
+  return (
+    <label className={classes.myLabel} htmlFor={htmlFor}>
+      {labeltext}
+    </label>
+  );
+};
+
+export default MyLabel;
